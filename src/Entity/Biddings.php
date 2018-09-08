afa05e4bcf361d3a43f762d051337fce1c637e80
Cosmin Pavel | eMAG, Software Dev Commercial <cosmin.pavel@emag.ro>
2018-09-08 20:48:32 +0300

    DatabaseBiddings

--- src/Entity/Biddings.php
+++ src/Entity/Biddings.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BiddingsRepository")
@@ -20,4 +22,57 @@ class Biddings
     {
         return $this->id;
     }
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $product_id;
+
+    /**
+     * @return integer
+     */
+
+    public function getProductId()
+    {
+        return $this->product_id;
+    }
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bid;
+
+    /**
+     * @return integer
+     */
+    public function getBid()
+    {
+        return $this->bid;
+    }
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $max_bid;
+
+    /**
+     * @return integer
+     */
+    public function getMaxBid()
+    {
+        return $this->max_bid;
+    }
+
 }
